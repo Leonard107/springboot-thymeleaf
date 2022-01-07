f80a295751e8daa76a10089dee98cb35c7025eaf
Leonard107 <leonardrichard107@gmail.com>
2022-01-06 23:38:09 -0300

    Criando um filtro pelo nome das pessoas

--- src/main/java/springboot/thymeleaf/springboot/thymeleaf/controller/PessoaController.java
+++ src/main/java/springboot/thymeleaf/springboot/thymeleaf/controller/PessoaController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import springboot.thymeleaf.springboot.thymeleaf.model.Pessoa;
@@ -66,5 +68,13 @@ public class PessoaController {
 		andView.addObject("pessoas", pessoaRepository.findAll());
 		andView.addObject("pessoaobj", new Pessoa());
 		return andView;	
+	}
+	
+	@PostMapping("**/pesquisarpessoa")
+	public ModelAndView pesquisar(@RequestParam("nomepesquisa") String nomepesquisa) {
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		andView.addObject("pessoas", pessoaRepository.findPessoaByName(nomepesquisa));
+		andView.addObject("pessoaobj", new Pessoa());
+		return andView;
 	}
 }
--- src/main/java/springboot/thymeleaf/springboot/thymeleaf/repository/PessoaRepository.java
+++ src/main/java/springboot/thymeleaf/springboot/thymeleaf/repository/PessoaRepository.java
@@ -1,5 +1,8 @@
 package springboot.thymeleaf.springboot.thymeleaf.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,5 +12,7 @@ import springboot.thymeleaf.springboot.thymeleaf.model.Pessoa;
 @Repository
 @Transactional
 public interface PessoaRepository extends CrudRepository<Pessoa, Long> {
-
+	
+	@Query("select p from Pessoa p where p.nome like %?1%")
+	List<Pessoa> findPessoaByName(String nome);
 }
